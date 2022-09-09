1e4d93715a0f35e8c487ef79cca99c68144d25c1
emmanuel-toast <emmanuel@toast.co.nz>
2022-09-09 16:15:14 +1200

    colour palette updates

--- dist/scripts/index.js
+++ dist/scripts/index.js
@@ -1,3 +1,31 @@
+class ColourPaletteController {
+  constructor() {
+    this.inputs = [];
+  }
+
+  update() {
+    [...document.querySelectorAll('.colorpalette')].forEach(container => {
+      const inputs = [...container.querySelectorAll('input')];
+
+      for (let index = 0; index < inputs.length; index++) {
+        const input = inputs[index];
+        const title = input.value.replace(/-/g, ' ');
+        const label = document.querySelector(`label[for="${input.id}"]`);
+        const span = document.createElement('span');
+        if (this.inputs.indexOf(input) > -1) continue;
+        span.innerHTML = title;
+        label.appendChild(span);
+        this.inputs.push(input);
+      }
+    });
+  }
+
+}
+
+const ColourPalette = new ColourPaletteController();
+ColourPalette.update();
+window.addEventListener('page-updated', () => ColourPalette.update());
+
 class DarkmodeController {
   constructor() {
     // The various mode settings
