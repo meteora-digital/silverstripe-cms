70d6ab0f3765d2daf3da25b4f1c70955afb27637
emmanuel-toast <emmanuel@toast.co.nz>
2023-05-01 15:57:03 +1200

    adding subtree to the page observer class as well as a throttle as i have found that some areas in the CMS change without causing the mutation observer to fire

--- dist/scripts/index.js
+++ dist/scripts/index.js
@@ -221,16 +221,22 @@ exports.IconFieldController = IconFieldController;
 class PageObserverController {
   constructor() {
     // Create a new event to fire
-    const e = new Event('page-updated'); // We are going to detect changes to the page and update functionality accordingly.
+    const e = new Event('page-updated');
+    let throttle = null; // We are going to detect changes to the page and update functionality accordingly.
 
     const Observer = new MutationObserver(() => {
-      // dispatch a new event to the window
-      window.dispatchEvent(e);
+      // Clear the throttle
+      clearTimeout(throttle); // Set a new throttle
+
+      throttle = setTimeout(() => {
+        // dispatch a new event to the window
+        window.dispatchEvent(e);
+      }, 100);
     }); // Observe the body
 
     Observer.observe(document.body, {
-      childList: true // subtree: true,
-
+      childList: true,
+      subtree: true
     });
   }
 
